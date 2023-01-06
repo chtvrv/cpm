98af48da4395212a08151e5b41bbfdd3539f655e
Tony Ch <678summerdays@gmail.com>
2023-01-06 13:25:29 +0300

    Add cli args processing (#2)

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	cpm_builder "cpm/builder"
-	cpm_parser "cpm/parser"
+	cpmbuilder "cpm/builder"
+	cpmparser "cpm/parser"
+	"flag"
 	"fmt"
 	s "github.com/inancgumus/prettyslice"
 )
 
+var args struct {
+	inputFilePath string
+}
+
 func main() {
-	parser := cpm_parser.CreateParser()
-	works := parser.Parse("example_path")
-	builder := cpm_builder.CreateBuilder()
-	adj_matrix, inputs, outputs := builder.BuildAdjacencyMatrix(works)
+	flag.StringVar(&args.inputFilePath, "c", "", "Path to the input file")
+	flag.Parse()
+
+	parser := cpmparser.CreateParser()
+	works := parser.Parse(args.inputFilePath)
+	builder := cpmbuilder.CreateBuilder()
+	adjMatrix, inputs, outputs := builder.BuildAdjacencyMatrix(works)
 	s.MaxPerLine = 30
 	s.PrintHex = true
-	for i := range adj_matrix.Data {
-		s.Show("row", adj_matrix.Data[i])
+	for i := range adjMatrix.Data {
+		s.Show("row", adjMatrix.Data[i])
 	}
 	fmt.Println(inputs, outputs)
 }
